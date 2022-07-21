fdd425627f781d119f7793783cf16a328c9ba942
zicstardust <zicstardust@outlook.com>
2022-07-21 19:46:25 -0300

    init commit 0.1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Minecraft-Backup-Gdrive
+Create tar.gz archive content a world and make upload to Google Drive
 Backup Minecraft Maps to Google Drive
---
+++ core.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+export HORARIO=$(date +%y-%m-%d.%H.%M)
+export ID_DIR_GDRIVE="1M40tz-AjIPcIxEsZOU7mJ9OQm28IoUiR"
+export DIR_MINECRAFT_MAP="${HOME}/.minecraft/saves"
+export TARGZ_TEMP="/tmp/minebackup.tar.gz"
+export PROCESS_NAME="minecraft-launcher"
+export LOG="/tmp/${HORARIO}.log"
+
+backup(){
+	echo "****************************************************INICIANDO BACKUP**************************************************" >> ${LOG}
+	if pidof -x "${PROCESS_NAME}"; then
+		echo "Game em executação no momento, nao foi possivel realizar backup no momento" >> ${LOG}
+	else
+		echo "****************************************************COMPACTANDO MAPAS**************************************************" >> ${LOG}
+		tar -czvf ${TARGZ_TEMP} ${DIR_MINECRAFT_MAP} >>${LOG}
+		gdrive upload -p ${ID_DIR_GDRIVE} --name ${HORARIO}.tar.gz ${TARGZ_TEMP} >>${LOG}
+		echo "************************************************BACKUP FINALIZADO**********************************************" >> ${LOG}
+		gdrive upload -p ${ID_DIR_GDRIVE} --name ${HORARIO}.log ${LOG} >/dev/null
+	fi
+}
