cd7b969cac678665612dc7f55f411e5bb1a91fbd
zicstardust <zicstardust@outlook.com>
2022-07-22 19:06:11 -0300

    0.3 - Reimpremented autodelete old backups

--- core.sh
+++ core.sh
@@ -6,11 +6,12 @@ export TARGZ_TEMP="/tmp/minebackup.tar.gz"
 export PROCESS_NAME="minecraft-launcher"
 export LOG="/tmp/${HORARIO}.log"
 export RETENCAO_DATA=$(date +%Y-%m-%d --date "7 days ago")
+export NAME_DIR_DRIVE=$(gdrive list | awk '$1 == "${ID_DIR_GDRIVE}" {print $2}')
 #source ./main.sh
 
 backup(){
 	echo "***************************************************INICIANDO BACKUP***************************************************" >> ${LOG}
-	if pidof -x "${PROCESS_NAME}"; then
+	if pidof -x "${PROCESS_NAME}" >/dev/null; then
 		echo "Game em executação no momento, nao foi possivel realizar backup" >> ${LOG}
 	else
 		echo "***************************************************COMPACTANDO MAPAS***************************************************" >> ${LOG}
@@ -21,14 +22,21 @@ backup(){
 }
 
 delete_old_backups(){
-	if awk '$7 > "${RETENCAO_DATA}" {print $1}'; then
-		echo "***************************************************DELETANDO BACKUPS COM MAIS DE 7 DIAS***************************************************" >> ${LOG}
-		gdrive list | awk '$7 <= "${RETENCAO_DATA}" {print $1}' | xargs gdrive delete >>${LOG}
+	echo "***************************************************DELETANDO BACKUPS ANTIGOS***************************************************" >> ${LOG}
+	if  gdrive list --absolute | awk '$2 ~ /${NAME_DIR_DRIVE}/ && $3 == "bin" && $6 <= "${RETENCAO_DATA}" {print $1}' >/dev/null; then
+		if gdrive list --absolute | awk '$2 ~ /${NAME_DIR_DRIVE}/ && $3 == "bin" && $6 > "${RETENCAO_DATA}" {print $1}' >/dev/null; then
+			gdrive list --absolute | awk '$2 ~ /${NAME_DIR_DRIVE}/ && $3 == "bin" && $6 <= "${RETENCAO_DATA}" {print $1}' | xargs gdrive delete >>${LOG}
+		else
+			echo "Não existem backups mais recentes que ${RETENCAO_DATA}, não serao apagado os backups antigos" >> ${LOG}
+		fi
+		echo "Não exixtem backups anteriores a ${RETENCAO_DATA} para deletar" >> ${LOG}
+	else
 	fi
 }
 
+#MAIN FUNCTION
 backup
 delete_old_backups
 echo "***************************************************BACKUP FINALIZADO***************************************************" >> ${LOG}
-gdrive upload -p ${ID_DIR_GDRIVE} --name ${HORARIO}.log --delete ${LOG} >/dev/null
+gdrive upload -p ${ID_DIR_GDRIVE} --name ${HORARIO}.log --delete ${LOG}
 echo "Backup finalizado"
